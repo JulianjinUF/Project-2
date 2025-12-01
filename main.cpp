df6fe8f27d43af6e31fba1416bbf044e38cf9b77
JulianjinUF <julianjin@csu.fullerton.edu>
2025-12-01 13:30:41 -0800

    Added some TODO for other members to do while i finsh the skeleton

--- main.cpp
+++ main.cpp
@@ -19,5 +19,26 @@ TableKey makeKey (char nonterminal, char terminal){
 // This will be the predictive parser class
 class PredictiveParser{
 public:
+// This one here is the Constructor. Sets up the stack and the parsing tabele
+PredictiveParser(){
+        parsingStack.push_back('$');
+        parsingStack.push_back('E');
+
+        intParsingTable(); // TODO: I will leave this to someeone to do take the parsing table from the PDF and the convert the rows and columns into key-value mappings.
+    }
+bool parse(const std::string &input);
+private:
+
+// Data member sections
+
+// This will be for the LL(1) parsing where we get the top of the stack = last element in the vector
+std::vector<char> parsingStack;
+
+// Parsing table: (Nonterminal, lookahead terminal) 
+// TODO: Someone wukk fill this table in intParsingTable().
+std::unordered_map<TableKey, Production RHS> parsingTable;
+
+
+
 
 }
